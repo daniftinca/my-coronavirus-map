9fc79fde2fa5f5ed8f52441e2692727fb9ab71ca
Dan <daniftincaalexandru@gmail.com>
2020-06-10 19:51:16 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Container from 'components/Container';
+
+const Header = () => {
+  return (
+    <header>
+      <Container type="content">
+        <p>Active Coronavirus Cases in the world</p>
+
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
